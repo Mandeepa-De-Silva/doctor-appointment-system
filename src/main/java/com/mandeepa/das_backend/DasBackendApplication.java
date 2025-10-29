ddbc29c45ff092c9a16734e3ce64d420b4b30e7b
Mandeepa De Silva <131390063+Mandeepa-De-Silva@users.noreply.github.com>
2025-10-29 14:50:37 +0530

    initial commit with project dependencies

---
+++ src/main/java/com/mandeepa/das_backend/DasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mandeepa.das_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DasBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mandeepa/das_backend/DasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mandeepa.das_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
