a366c04ce545e143617fd27e765e6d66c7dc0975
Mandeepa De Silva <131390063+Mandeepa-De-Silva@users.noreply.github.com>
2025-10-30 09:13:10 +0530

    feat: add DuplicateFoundException and enhance global exception handling

--- src/main/java/com/mandeepa/das_backend/advice/GlobalExceptionHandler.java
+++ src/main/java/com/mandeepa/das_backend/advice/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.mandeepa.das_backend.advice;
 
+import com.mandeepa.das_backend.exception.DuplicateFoundException;
 import com.mandeepa.das_backend.exception.ErrorResponse;
 import com.mandeepa.das_backend.exception.ResourceNotFoundException;
 import com.mandeepa.das_backend.exception.UnAuthorizedException;
@@ -7,8 +8,12 @@ import io.swagger.v3.oas.annotations.Hidden;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Hidden
 @ControllerAdvice
@@ -36,5 +41,26 @@ public class GlobalExceptionHandler {
                 request.getRequestURI()
         );
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(DuplicateFoundException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgument(
+            DuplicateFoundException ex,
+            HttpServletRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.CONFLICT,
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
