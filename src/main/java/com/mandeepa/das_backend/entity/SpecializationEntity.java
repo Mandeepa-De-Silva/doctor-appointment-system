598c73bdb5b9590919b52373e21ea11ebd2722ee
Mandeepa De Silva <131390063+Mandeepa-De-Silva@users.noreply.github.com>
2025-11-04 22:16:30 +0530

    feat: add Specialization entity and DTOs for specialization management

---
+++ src/main/java/com/mandeepa/das_backend/entity/SpecializationEntity.java
@@ -0,0 +1,19 @@
+package com.mandeepa.das_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "specialization", uniqueConstraints = @UniqueConstraint(columnNames = "name"))
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SpecializationEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false, unique = true)
+    private String name;
+}
