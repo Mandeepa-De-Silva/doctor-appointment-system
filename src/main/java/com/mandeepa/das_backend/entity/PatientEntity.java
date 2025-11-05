66f73458ce8d2329957264817f2c37e7dfb16bda
Mandeepa De Silva <131390063+Mandeepa-De-Silva@users.noreply.github.com>
2025-11-05 19:47:01 +0530

    feat: add Patient entity and DTOs for patient management

---
+++ src/main/java/com/mandeepa/das_backend/entity/PatientEntity.java
@@ -0,0 +1,26 @@
+package com.mandeepa.das_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name="patient")
+public class PatientEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(optional = false)
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private UserEntity user;
+
+    private String phone;
+    private String dob;
+}
