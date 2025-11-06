714add3f1c54da2dfaeea0ef8cd119f0c9878a47
Mandeepa De Silva <131390063+Mandeepa-De-Silva@users.noreply.github.com>
2025-11-06 08:48:03 +0530

    feat: add Patient repository and service interface for patient management

---
+++ src/main/java/com/mandeepa/das_backend/service/patient/PatientService.java
@@ -0,0 +1,10 @@
+package com.mandeepa.das_backend.service.patient;
+
+import  com.mandeepa.das_backend.dto.patient.*;
+public interface PatientService {
+
+    PatientMeResponse me(String username);
+
+    PatientMeResponse updateMe(String username, PatientUpdateRequest req);
+}
+
